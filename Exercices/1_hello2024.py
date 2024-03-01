1cb36965ff289ff938e90d066c02162de5f7756b
Ângelo Matins Finassi <159829035+AngeloFinassi@users.noreply.github.com>
2024-03-01 16:19:18 -0300

    my first exercices in python

---
+++ Exercices/1_hello2024.py
@@ -0,0 +1,8 @@
+#Fuck you printf + scanf, pqp a alegria mano
+name  = str(input("Name: "))
+
+#Three form to print
+print("Hello word")
+print("Welcome",name)
+print("Welcome " + name)
+print(f"Welcome {name}")
---
+++ Exercices/2_varaibles.py
@@ -0,0 +1,5 @@
+#we have types of variable
+# bool - True or false,  0 and 1
+# float  - 0.xtyz
+# int - 1, 2, 3, 4
+# str - letters sdadas 'Elon musk is gay!'
