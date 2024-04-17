739bd564dabe48cbe0c2551fc24e6eca84f2f94e
Ângelo Matins Finassi <159829035+AngeloFinassi@users.noreply.github.com>
2024-04-17 14:08:39 -0300

    leet code exercício

--- desafios/easy/Duplicate Zeros.py
+++ desafios/easy/Duplicate Zeros.py
@@ -1,6 +1,14 @@
+"
 arr = [1,0,2,3,0,4,5,0]
-# output ->[1,0,0,2,3,0,0,4]
 
-
-# return arr
-print(arr)
+arr_clone = []
+for c in arr:
+    if len(arr_clone) < len(arr):
+        if c == 0:
+            arr_clone.append(c)
+            arr_clone.append(c)
+        else:
+            arr_clone.append(c)
+arr = arr_clone.copy()
+print(arr_clone)
+"
