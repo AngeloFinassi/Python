9d7eb1ad76b97d9771750f2a03ad3a154b28b556
Ângelo Matins Finassi <159829035+AngeloFinassi@users.noreply.github.com>
2024-05-03 11:59:49 -0300

    arrumando pastas e arquivos

--- Exercise/mario.py
+++
@@ -1,9 +0,0 @@
-while True:
-    height = int(input("Height: "))
-    if height > 0:
-        break
-
-for c in range(height):
-    for j in range(height):
-        print("#", end  = "")
-    print()
