5547d445fd00d93d611add9f00fc7e0403d67486
Ângelo Matins Finassi <159829035+AngeloFinassi@users.noreply.github.com>
2024-05-23 15:22:17 -0300

    selection sort feito, upload

--- desafios/easy/sort.py
+++ desafios/easy/sort.py
@@ -1,17 +1,43 @@
 import random
 
-numbers = [random.randint(1, 1000) for _ in range(10)]
+numbers = [random.randint(1, 100) for _ in range(5)]
+
+def BigSmal(numbers, param):
+    if not numbers:
+        return None, None
+    
+    n_bigger = n_smaller = numbers[0]
+
+    for c in range(0, len(numbers)): 
+        n = numbers[c]
+        if n > n_bigger:
+            n_bigger = n
+        if n < n_smaller:
+            n_smaller = n
+    if param == 1:
+        return n_bigger
+    else:
+        return n_smaller
 
 def bubble_sort(numbers):
     length = len(numbers)
     for i in range(length):
-        # range(0, n - i - 1) diminui a cada iteração do loop externo
+        # range(0, length - i - 1) diminui a cada iteração do loop externo
         for k in range(0 , length - i - 1):
             if numbers[k] > numbers[k + 1]:
                 numbers[k], numbers[k + 1] = numbers[k + 1], numbers[k]
     return numbers
 
 
-bubble_sort(numbers)
-print(numbers)
+def selection(numbers):
+    for i in range(len(numbers)):
+        min_index = i
+        for k in range(1 + i, len(numbers)):
+            if numbers[k] < numbers[min_index]:
+                min_index = k
+        numbers[min_index], numbers[i] = numbers[i], numbers[min_index]
+    return numbers
 
+
+print(numbers)
+print(selection(numbers))
--- desafios/sorted_game.py
+++ desafios/sorted_game.py
@@ -8,6 +8,29 @@ def draw():
         n = numbers[c]
         pygame.draw.rect(screen, (255, 255, 255), (c* size, screen_height - n, size, n))
 
+def bubble():
+    length = len(numbers)
+    for i in range(length):
+        # range(0, n - i - 1) diminui a cada iteração do loop externo
+        for k in range(0 , length - i - 1):
+            draw()
+            if numbers[k] > numbers[k + 1]:
+                numbers[k], numbers[k + 1] = numbers[k + 1], numbers[k]
+                pygame.display.flip()
+                time.sleep(times)
+
+def selection(numbers):
+    for i in range(len(numbers)):
+        min_index = i
+        for k in range(1 + i, len(numbers)):
+            draw()
+            if numbers[k] < numbers[min_index]:
+                min_index = k
+                pygame.display.flip()
+                time.sleep(times)
+        numbers[min_index], numbers[i] = numbers[i], numbers[min_index]
+        
+
 # Define as dimensões da janela
 screen_widht, screen_height = 1920, 800
 screen = pygame.display.set_mode((screen_widht, screen_height))
@@ -24,9 +47,7 @@ numbers = []
 temporary_size = screen_height
 
 #numberList
-for k in range(0, int(screen_widht / size)):
-    temporary_size -= size
-    numbers.append(temporary_size)
+numbers = [screen_height - size * k for k in range(int(screen_widht / size))]
 random.shuffle(numbers)
 
 
@@ -37,16 +58,9 @@ while True:
             sys.exit()
     clock.tick(FPS)
 
-    length = len(numbers)
-    for i in range(length):
-        # range(0, n - i - 1) diminui a cada iteração do loop externo
-        for k in range(0 , length - i - 1):
-            draw()
-            if numbers[k] > numbers[k + 1]:
-                numbers[k], numbers[k + 1] = numbers[k + 1], numbers[k]
-                pygame.display.flip()
-                time.sleep(times)
+    #bubble(numbers)
+    selection(numbers)
+    print(numbers)
+    time.sleep(3)
     break
-
-    pygame.display.flip()
     
