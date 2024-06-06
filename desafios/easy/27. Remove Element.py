6b8e49c82f9e91e0c5adf3d3838c6aa4e85afffc
Ângelo Matins Finassi <159829035+AngeloFinassi@users.noreply.github.com>
2024-06-06 14:19:02 -0300

    refazendo desafio 27 python, mais eficiente

--- desafios/easy/27. Remove Element.py
+++ desafios/easy/27. Remove Element.py
@@ -2,13 +2,12 @@ nums = [3,2,2,3]
 val = 2
 k = 0
 
-l = 0
-for j in range(0, len(nums)):
-    if nums[j] == val:
-        l += 1
-k = len(nums) - l
-
-for i in range(0, l):
-    nums.remove(val)
+j = 0
+while j < len(nums):
+    if nums[j] != val:
+        k += 1
+        j += 1
+    else:
+        nums.remove(val)
 
 print(k, nums)
