26e5c135de53c42e3d10bca4ec75bd2e7b40c57c
Ângelo Matins Finassi <159829035+AngeloFinassi@users.noreply.github.com>
2024-09-17 14:54:28 -0300

    bcc upload projeto final

--- bcc/pandas/8.correlação.py
+++ bcc/pandas/8.correlação.py
@@ -1,23 +1,14 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
-def convert_to_float(df):
-    #Substituir vírgulas por pontos apenas em colunas de strings e depois converter para float
-    for col in df.columns:
-        #Verifica se a coluna é do tipo string
-        if df[col].dtype == 'object':
-            df[col] = df[col].str.replace(',', '.').astype(float)
-    return df
+#Criação dos dados
+data = {
+    #         2018, 2019, 2020, 2021, 2022, 2023
+    'ipca' : [3.75, 4.31, 4.52, 10.06, 5.79, 3.94],
+    'alimentacao' : [4.04, 6.37, 14.09, 7.94, 11.64, 5.47]
+    }
 
-df1 = pd.read_csv(r"C:\Users\finas\OneDrive\Documentos\int.csv", sep=";", index_col=0)
-df2 = pd.read_csv(r"C:\Users\finas\OneDrive\Documentos\freq_estudos_dps_em.csv", sep=";", index_col=0)
+#Conversão para um Data Frame
+df = pd.DataFrame(data)
 
-df1 = convert_to_float(df1)
-df2 = convert_to_float(df2)
-
-# Alinhar os DataFrames pelos índices
-merged_df = pd.merge(df1, df2, left_index=True, right_index=True, how='inner')
-
-# Calcular a correlação entre as colunas
-correlation = merged_df.corr().iloc[0, 1]  # Correlação entre a primeira coluna de df1 e df2
-print(f'Correlação entre os dois gráficos: {correlation}')
+correlation = df["ipca"].corr(df["alimentacao"])
+print(correlation)
